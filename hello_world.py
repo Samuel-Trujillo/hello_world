67b598f7da0b428bb9cca3d4bd57d6ce331300b3
Samuel-Trujillo <samuel.trujillo093@gmail.com>
2021-05-03 12:48:44 -0700

    commit 1

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+
+print("Hello World")
+
+name = "Noelle"
+print("Hello",name + "!")	
+print("Hello " + name +"!")
+name = 42
+print("Hello",name)	
+print("Hello",str(name))
+
+food1 = "sushi"
+food2 = "pizza"
+print("I love to eat {} and {}".format(food1,food2)) 
+print(f"I love to eat {food1} and {food2}") 
